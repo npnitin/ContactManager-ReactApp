8b60a20ef1154a8a3970ecee9b95390913087b36
np85075 <pawarnitin@johndeere.com>
2019-02-14 16:25:46 +0530

    adds TextInputGropu component for DRY(DO NOt Repeat) code

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Consumer } from '../context/context';
 import uuid from 'uuid';
+import TextInputGroup from '../layout/TextInputGroup';
+
 class AddContact extends Component {
     constructor(){
         super();
@@ -13,7 +15,7 @@ class AddContact extends Component {
     onChange = (e) =>{
         this.setState({
             [e.target.name]:e.target.value
-        })
+        });
     }
     onSubmit = (dispatch,e) =>{
         e.preventDefault();
@@ -43,39 +45,28 @@ class AddContact extends Component {
                     <div className="card-header">Add Contact</div>
                         <div className="card-body">
                             <form onSubmit={this.onSubmit.bind(this,dispatch)}>
-                                <div className="form-group">
-                                    <label htmlFor="name">Name</label>
-                                    <input 
-                                    type="text" 
-                                    className="form-control form-control-lg"
-                                    name="name"
-                                    placeholder="Enter your name.."
-                                    value={name}
-                                    onChange={this.onChange}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="name">Email</label>
-                                    <input 
-                                    type="email" 
-                                    className="form-control form-control-lg"
-                                    name="email"
-                                    placeholder="Enter your email.."
-                                    value={email}
-                                    onChange={this.onChange}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="name">Phone</label>
-                                    <input 
-                                    type="text" 
-                                    className="form-control form-control-lg"
-                                    name="phone"
-                                    placeholder="Enter your phone.."
-                                    value={phone}
-                                    onChange={this.onChange}
-                                    />
-                                </div>
+                                <TextInputGroup
+                                name="name"
+                                label="Name"
+                                placeholder="Enter Name"
+                                value={name}
+                                onChange={this.onChange}
+                                />
+                                 <TextInputGroup
+                                name="email"
+                                label="Email"
+                                placeholder="Enter email"
+                                type="email"
+                                value={email}
+                                onChange={this.onChange}
+                                />
+                                 <TextInputGroup
+                                name="phone"
+                                label="Phone"
+                                placeholder="Enter phone"
+                                value={phone}
+                                onChange={this.onChange}
+                                />
                                 <input type="submit" value="Add Contact" className="btn btn-light btn-block"/>
                             </form>
                         </div>
