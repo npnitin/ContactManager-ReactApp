85e5de610ac0f0794fc1edc8b7cba24c7fbbaf7c
np85075 <pawarnitin@johndeere.com>
2019-02-15 14:11:38 +0530

    adds routing in react

---
+++ src/components/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">About contact manager</h1>
+      <p>simple app to manage contacts</p>
+      <p className="text-secondary">Version 1.0.0</p>
+    </div>
+  )
+}
